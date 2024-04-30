fe1c6041e629adfa406a9e20a603fa7bfd804286
Mahbub Mohsin <mollahmahbub81@gmail.com>
2024-04-30 17:31:27 +0600

    37 commit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -42,45 +42,6 @@ const [loading, setLoading]=useState(false)
   return (
     <main className="flex min-h-screen flex-col w-full  ">
 
-      <div className="w-full h-[60px] bg-white flex flex-row  justify-between">
-         <div className="">
-            <Image src='/logo.png' height={40} width={160} alt="ff"/>
-         </div>
-         <div>
-         <button className="px-3 py-2 text-white m-2 bg-red-500 rounded-md" onClick={() => signOut()}>Sign out</button> 
-         </div>
-      </div>
-       <div className="w-full p-3">
-       <div className="w-full  m-3 p-3 mx-auto bg-slate-200 dark:bg-slate-500 flex flex-row justify-start">
-         <div>
-          {user.gender!="" ? <Image src={user.gender==="female" ?"/female_avatar.jfif":"/male_avatar.jfif"} height={100} width={100} alt="Student"/>:<div className="w-[100px] h-[100px] flex flex-col justify-center text-gray-300"><div>Loading...</div></div>}
-         </div>
-         <div className="h-full  items-center flex flex-col justify-center relative">
-           <div className=" px-2 flex flex-col text-sm"> 
-            <span className="font-bold"> Name : {user.name}</span>
-            <span> Id : {user.email}</span>
-            <span> Class : {user.class_type}</span>
-            <span> Due : {user.payment_due}</span>
-            <span>Total Class : {}</span>
-           </div>
-          
-         </div>
-         
-      </div> 
-       </div>
-      
-        
-       <div className="w-full p-3">
-        <div className="w-full  p-3 mx-auto bg-slate-200 dark:bg-slate-500 flex flex-col justify-start">
-        <DailyWork />
-          </div>
-          </div>
-      <div className="w-full p-3">
-        <div className="w-full  p-3 mx-auto bg-slate-200 dark:bg-slate-500 flex flex-col justify-start">
-        <Syllebus />
-         </div>
-        </div>
-      
     </main>
   );
 }
